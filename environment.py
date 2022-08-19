070deebb946f1ffa7c4a9b580300b023e3c03997
Chukwunedum Ekeruche <103284824+Chinedu-E@users.noreply.github.com>
2022-08-18 22:39:23 -0500

    Update environment.py
    changed image dimension to be passed to dqn
    coin ocr was too slow, removed for now.

--- environment.py
+++ environment.py
@@ -6,15 +6,16 @@ import numpy as np
 from gym import Env
 from gym.spaces import Discrete, Box
 from utils import screenshot, read_coins
-from keras.models import load_model
+import train_endgame as eg
 
 
 class SubwayEnv(Env):
 
     def __init__(self):
         self.action_space = Discrete(5)
-        self.observation_space = Box(low=0, high=255, shape=(150, 150, 1), dtype=np.uint64)
-        self.game_over_model = load_model('endgame1.h5')
+        self.observation_space = Box(low=0, high=255, shape=(80, 120, 3), dtype=np.uint64)
+        self.game_over_model = eg.build()
+        self.game_over_model.load_weights('endgame1.h5')
         self.coins = 0
 
     def step(self, action):
@@ -27,49 +28,58 @@ class SubwayEnv(Env):
 
         if action != 'nothing':
             keyboard.press_and_release(action)
-
-        time.sleep(4 / 15)
+        time.sleep(0.125)
         state = screenshot()
-        pyautogui.click(426, 198)
+        # pyautogui.click(426, 198)
         reward, done = self.reward_function(state)
         info = {}
 
         return state, reward, done, info
 
     def reset(self):
-        # TO-DO
-        ...
+        start_time = time.time()
+        while True:
+            pyautogui.click(358, 800, clicks=3, interval=1.0)
+            if (time.time() - start_time) > 3:  # failsafe triggered after 4 seconds of resetting
+                pyautogui.click(426, 198)
+                time.sleep(0.5)
+                pyautogui.click(358, 800, clicks=3, interval=1.0)
+
+            state = screenshot()
+            if self.is_game_over(state):  # clicking till we are back to game screen
+                pass
+            else:
+                break
+        return state
 
     def render(self):
         ...
 
-    def start(self):
-        ...
-
     def reward_function(self, state):
-        coins = read_coins()
+        #coins = read_coins()
         done = self.is_game_over(state)
         if done:
-            reward = -15
-            return reward, done
-
-        ep_diff = coins - self.coins
-        if ep_diff > 1:
-            reward = 2
-        else:
             reward = -1
+            return reward, done
+        reward = 0.5
 
-        self.coins = coins
+        #ep_diff = coins - self.coins
+        #if ep_diff > 1:
+        #    reward = -2
+        #else:
+        #    reward = 1
+        #self.coins = coins
 
         return reward, done
 
     def is_game_over(self, state) -> bool:
         state = cv2.resize(state, (96, 96))
+        state = np.float32(state)
+        state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
         state = np.expand_dims(state, -1)
-        classes = ['end', 'in-game']
-        pred = self.game_over_model.predict(state, verbose=0)[0]
-        pred = classes[np.argmax(pred, axis=-1)]
-        if pred == 'end':
+        state = np.expand_dims(state, 0)
+        pred = self.game_over_model.predict(state, verbose=0)[0][0]
+        if pred < 0.001:
             return True
         else:
             return False
