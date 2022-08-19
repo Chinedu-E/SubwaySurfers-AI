ed7bac6cc174e11410353aaaba009f221015f422
Chukwunedum Ekeruche <103284824+Chinedu-E@users.noreply.github.com>
2022-08-18 22:36:36 -0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -8,13 +8,36 @@ from utils import NoisyDense
 from buffer import MemoryBuffer
 
 
-class Agent:
-    def __init__(self, discount_rate=0.95, learning_rate=6e-5,
-                 with_per=True, batch_size=129,
-                 epsilon=0.0, epsilon_decay=0.0,
-                 epsilon_min=0.0, target_update_freq=10):
+from keras.layers import Dense, \
+    Flatten, Input, LeakyReLU, Lambda, ReLU, ConvLSTM2D
+from keras.models import Model
+from keras.losses import MSE
+from keras.optimizers import Adam
+import numpy as np
+import gym
+import tensorflow as tf
+from utils import NoisyDense
+from buffer import MemoryBuffer
 
-        self.input_dim = (150, 150, 1)
+
+class Agent:
+    def __init__(self, n_frames_stack: int,
+                 env: gym.Env,
+                 discount_rate=0.99,
+                 learning_rate=1e-5,
+                 with_per: bool = True,
+                 batch_size: int = 32,
+                 epsilon=0.0,
+                 epsilon_decay=0.0,
+                 epsilon_min=0.0,
+                 target_update_freq: int = 4000,
+                 buffer_size: int = 100000,
+                 optimizer_epsilon: float = 1.5e-4
+                 ):
+        self.n_frames_stack = n_frames_stack
+        # shape of (n_stacked_frames, width, height, channels)
+        self.input_dim = (n_frames_stack, *env.observation_space.shape)
+        self.n_actions = env.action_space.shape[0]
         self.eps_decay = epsilon_decay
         self.eps_min = epsilon_min
         self.epsilon = epsilon
@@ -23,38 +46,54 @@ class Agent:
         self.gamma = discount_rate
         self.learning_rate = learning_rate
         self.target_update_freq = target_update_freq
+        self.optimizer_epsilon = optimizer_epsilon
 
         model = self._build_model()
         target_model = self._build_model()
         target_model.set_weights(model.get_weights())
-        # model.load_weights(".h5")
+        
+        try:
+            model.load_weights("weights/dqn")
+            print("loaded existing weights")
+        except FileNotFoundError:
+            print("training from scratch")
+            
         self.model = model
         self.target_model = target_model
-        self.buffer = MemoryBuffer(100000, with_per)
+        self.buffer = MemoryBuffer(buffer_size, with_per)
         # Print the model summary if you want to see what it looks like
         print(self.model.summary())
 
-        self.loss = []
-        self.location = 0
-
     def _build_model(self):
         input_layer = Input(self.input_dim)
-        x = Conv2D(filters=32, kernel_size=8
-                   , strides=1, padding='same')(input_layer)
+        x = ConvLSTM2D(filters=32,
+                       kernel_size=8,
+                       strides=4,
+                       return_sequences=True,
+                       padding="same")(input_layer)
+        x = ConvLSTM2D(filters=64,
+                       kernel_size=4,
+                       strides=2,
+                       return_sequences=True,
+                       padding="same")(x)
         x = LeakyReLU(alpha=0.2)(x)
-        x = Conv2D(filters=64, kernel_size=5, strides=2, padding='same')(x)
+        x = ConvLSTM2D(filters=64,
+                       kernel_size=3,
+                       strides=1,
+                       return_sequences=False,
+                       padding="same")(x)
         x = LeakyReLU(alpha=0.2)(x)
-        x = Conv2D(filters=128, kernel_size=3, strides=1, padding='same')(x)
+        x = Flatten()(x)
+        x = Dense(512)(x)
         x = LeakyReLU(alpha=0.2)(x)
-        x = GlobalAvgPool2D()(x)
-        x = Dense(64)(x)
-        x = LeakyReLU(alpha=0.2)(x)
-        x = Dropout(rate=0.1)(x)
-        value_fc = NoisyDense(32)(x)
+        value_fc = NoisyDense(128)(x)
+        value_fc = ReLU()(value_fc)
         value = NoisyDense(1)(value_fc)
 
-        advantage_fc = NoisyDense(32)(x)
-        advantage = NoisyDense(5)(advantage_fc)
+        advantage_fc = NoisyDense(128)(x)
+        advantage_fc = ReLU()(advantage_fc)
+        advantage = NoisyDense(self.n_actions)(advantage_fc)
+        self.advantage = Model(input_layer, advantage)
 
         def d_output(args):
             a = args[0]
@@ -64,7 +103,8 @@ class Agent:
         output = Lambda(d_output)([advantage, value])
 
         model = Model(input_layer, output)
-        model.compile(loss='mean_squared_error', optimizer=Adam(learning_rate=self.learning_rate, epsilon=1.5e-4))
+        model.compile(optimizer=Adam(learning_rate=self.learning_rate, epsilon=self.optimizer_epsilon),
+                      loss="mse")
 
         return model
 
@@ -72,7 +112,7 @@ class Agent:
         """ predict next action from Actor's Policy
         """
         obs = np.expand_dims(obs, axis=0)
-        qvals = self.predict(obs)[0]
+        qvals = self.predict(obs, advantage=True)[0]
         # Exploitation
         action = np.argmax(qvals)  # Highest Q-value
 
@@ -98,24 +138,26 @@ class Agent:
             # sample from buffer
             states, actions, rewards, dones, new_states, idx = self.sample_batch(self.batch_size)
 
-            # get target q-value using target network
             states = tf.convert_to_tensor(states, dtype=tf.float32)
             new_states = tf.convert_to_tensor(new_states, dtype=tf.float32)
-            q_vals = self.target_predict(new_states)
 
-            # bellman iteration for target critic value
-            critic_target = np.asarray(q_vals)
+            q_pred = self.predict(states)
+            q_vals = self.target_predict(new_states)
+            past_actions = self.predict(new_states)
+            max_actions = tf.math.argmax(past_actions, axis=1)
+
+            target = q_pred.copy()
             for i in range(q_vals.shape[0]):
                 if dones[i]:
-                    critic_target[i] = rewards[i]
+                    target[i] = rewards[i]
                 else:
-                    critic_target[i] = rewards[i] + self.gamma * np.max(q_vals[i])
+                    target[i] = rewards[i] + self.gamma * q_vals[i, max_actions[i]]
 
                 if self.with_per:
-                    self.buffer.update(idx[i], abs(q_vals[i] - critic_target[i])[0])
+                    self.buffer.update(idx[i], abs(q_vals[i] - target[i])[0])
 
-            # train(or update) the actor & critic and target networks
-            loss = self.update_network(states, critic_target)
+            # training
+            loss = self.update_network(states, target)
             losses.append(loss)
         return losses
 
@@ -124,14 +166,17 @@ class Agent:
         """
         return self.buffer.sample_batch(batch_size)
 
-    def update_network(self, obs, critic_target):
-        history = self.model.fit(
-            obs, critic_target, epochs=1, verbose=0)
-        loss = history.history.get("loss")[0]
+    def update_network(self, obs, target):
+        """Train Q-network for critic on sampled batch
+        """
+        loss = self.model.train_on_batch(obs, target)
         return loss
 
-    def predict(self, state):
-        qval = self.model.predict(state, verbose=0)
+    def predict(self, state, advantage: bool = False):
+        if advantage:
+            qval = self.advantage.predict(state, verbose=0)
+        else:
+            qval = self.model.predict(state, verbose=0)
         return qval
 
     def target_predict(self, state):
